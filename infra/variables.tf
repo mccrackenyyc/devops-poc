83c09f199a23bab57ef542ff938d67b3ab7a79b4
mccrackenyyc <smccracken@live.ca>
2024-07-09 19:00:05 -0600

    Formatting

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,29 +1,29 @@
 variable "subscription_id" {
-    description = "Azure subscription"
-    type = string
-    default = "20c17ce1-c880-4374-ab18-0c3a72158cf7"
+  description = "Azure subscription"
+  type        = string
+  default     = "20c17ce1-c880-4374-ab18-0c3a72158cf7"
 }
 
 variable "location" {
-    description = "Primary datacenter"
-    type = string
-    default = "canadacentral"
+  description = "Primary datacenter"
+  type        = string
+  default     = "canadacentral"
 }
 
 variable "loc_short" {
-    description = "Primary datacenter shorthand"
-    type = string
-    default = "cc"
+  description = "Primary datacenter shorthand"
+  type        = string
+  default     = "cc"
 }
 
 variable "env" {
-    description = "Working environment"
-    type = string
-    default = "dev"
+  description = "Working environment"
+  type        = string
+  default     = "dev"
 }
 
 variable "project" {
-    description = "shorthand project codename"
-    type = string
-    default = "dvp"
+  description = "shorthand project codename"
+  type        = string
+  default     = "dvp"
 }
